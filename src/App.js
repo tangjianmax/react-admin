5f79ea5a6ce771a25ffdb97cbe78baffeb72666c
tangjian <1091478086@qq.com>
2019-08-13 18:05:27 +0800

    完成登录页面静态布局

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './pages/login'
+import Admin from './pages/admin'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return <Router>
+            <Switch>
+                <Route path="/login" component={Login}/>
+                <Route path="/" component={Admin}/>
+            </Switch>
+        </Router>
+
+    }
 }
 
 export default App;
---
+++ src/pages/login/index.js
@@ -0,0 +1,37 @@
+import React,{Component} from 'react';
+import { Form, Input, Icon, Button } from 'antd';
+import logo from './logo.png';
+import './index.less'
+const Item = Form.Item
+class Login extends Component{
+    render(){
+        return <div className="login">
+          <header className="login-header">
+              <img src={logo}/>
+              <h1>React:后台项目</h1>
+          </header>
+            <section className="login-section">
+                <div className="login-middle">
+                <h2>用户登录</h2>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Item>
+                            <Input
+                                prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                placeholder="用户名"/>,
+                        </Item>
+                        <Item>
+                            <Input
+                                prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                type="password"
+                                placeholder="密码"/>
+                        </Item>
+                        <Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">登录</Button>
+                        </Item>
+                    </Form>
+                </div>
+            </section>
+        </div>
+    }
+}
+export default Login;
